b84d86d5e6129696042a7386dc6b447c9d3c20b4
skanda99 <skandaupadhyaya1999@gmail.com>
2019-05-11 22:30:06 +0530

    ABC: 125-A

---
+++ A-BiscuitGenerator.cpp
@@ -0,0 +1,16 @@
+
+// problem: "https://atcoder.jp/contests/abc125/tasks/abc125_a"
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cin>>a>>b>>c;
+
+    cout<<b*(c/a);
+
+    return 0;
+}
