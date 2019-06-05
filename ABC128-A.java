776cef9b45da4f5df88db15fb3573ef3739ed6f6
skanda99 <skandaupadhyaya1999@gmail.com>
2019-06-05 11:14:33 +0530

    Apple Pie

---
+++ ABC128-A.java
@@ -0,0 +1,14 @@
+
+// problem: "https://atcoder.jp/contests/abc128/tasks/abc128_a"
+import java.util.Scanner;
+
+class Main {
+    public static void main (String args[]) {
+        Scanner in = new Scanner(System.in);
+
+        int A = in.nextInt();
+        int P = in.nextInt();
+
+        System.out.print((3*A+P)/2);
+    }
+}
