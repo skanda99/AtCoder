2232275878d60c5fdc6af52a275322f2905b7617
skanda99 <skandaupadhyaya1999@gmail.com>
2019-12-06 16:35:00 +0530

    Weather Prediction

---
+++ ABC141-A.cpp
@@ -0,0 +1,21 @@
+
+// problem: "https://atcoder.jp/contests/abc141/tasks/abc141_a"
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    cin>>s;
+
+    if(s == "Sunny")
+        cout<<"Cloudy";
+    else if(s == "Cloudy")
+        cout<<"Rainy";
+    else
+        cout<<"Sunny";
+
+    return 0;
+}
