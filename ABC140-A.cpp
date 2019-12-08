883fb7435a1a059f2e6c46a52f18f0b7b5ac16b8
skanda99 <skandaupadhyaya1999@gmail.com>
2019-12-08 23:53:48 +0530

    Password

---
+++ ABC140-A.cpp
@@ -0,0 +1,16 @@
+
+// problem: "https://atcoder.jp/contests/abc140/tasks/abc140_a"
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    cout<<n*n*n;
+
+    return 0;
+}
