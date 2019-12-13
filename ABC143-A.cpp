a182b1799177ecda481514572a17aea0160a32bc
skanda99 <skandaupadhyaya1999@gmail.com>
2019-12-13 11:18:33 +0530

    Curtain

---
+++ ABC143-A.cpp
@@ -0,0 +1,17 @@
+
+// problem: "https://atcoder.jp/contests/abc143/tasks/abc143_a"
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+
+    cin>>a>>b;
+
+    cout<<max(0,a-2*b);
+
+    return 0;
+}
