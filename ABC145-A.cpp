c516918dcec676fbd1d680c3cb381199d50b9770
skanda99 <skandaupadhyaya1999@gmail.com>
2019-12-28 11:29:07 +0530

    Circle

---
+++ ABC145-A.cpp
@@ -0,0 +1,16 @@
+
+// problem: "https://atcoder.jp/contests/abc145/tasks/abc145_a"
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int r;
+    cin>>r;
+
+    cout<<r*r;
+
+    return 0;
+}
