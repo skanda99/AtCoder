66a6c9ce088b730b954d051c20e09c2e788b4e1e
skanda99 <skandaupadhyaya1999@gmail.com>
2020-01-12 01:03:27 +0530

    Strings

---
+++ ABC149-A.cpp
@@ -0,0 +1,15 @@
+
+// problem: "https://atcoder.jp/contests/abc149/tasks/abc149_a"
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string s,t;
+    cin>>s>>t;
+
+    cout<<t+s;
+    return 0;
+}
