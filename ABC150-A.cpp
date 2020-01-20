ba773a540241fd19d734454a424e633b550945dc
skanda99 <skandaupadhyaya1999@gmail.com>
2020-01-20 14:08:38 +0530

    500 Yen Coins

---
+++ ABC150-A.cpp
@@ -0,0 +1,20 @@
+
+// problem: "https://atcoder.jp/contests/abc150/tasks/abc150_a"
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int k,x;
+    cin>>k>>x;
+
+    if(k*500 >= x)
+        cout<<"Yes";
+
+    else
+        cout<<"No";
+
+    return 0;
+}
