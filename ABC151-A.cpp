18c78873d7fec920b07e327e05dc633d4ea0a644
skanda99 <skandaupadhyaya1999@gmail.com>
2020-03-01 13:29:27 +0530

    Next Alphabet

---
+++ ABC151-A.cpp
@@ -0,0 +1,16 @@
+
+// problem: "https://atcoder.jp/contests/abc151/tasks/abc151_a"
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    char c;
+
+    cin>>c;
+    cout<<char(c+1);
+
+    return 0;
+}
