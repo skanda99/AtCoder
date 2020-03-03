468a709f2049f234b2bbf57e40d4c581525599ad
skanda99 <skandaupadhyaya1999@gmail.com>
2020-03-03 07:57:50 +0530

    AC or WA

---
+++ ABC152-A.cpp
@@ -0,0 +1,20 @@
+
+// problem: "https://atcoder.jp/contests/abc152/tasks/abc152_a"
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+
+    if(n == m)
+        cout<<"Yes";
+
+    else
+        cout<<"No";
+
+    return 0;
+}
