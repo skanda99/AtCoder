e29db66e8508226225812b4425e16fe2cbad5d22
skanda99 <skandaupadhyaya1999@gmail.com>
2020-03-24 16:58:31 +0530

    Duplex Printing

---
+++ ABC157-A.cpp
@@ -0,0 +1,20 @@
+
+// problem: "https://atcoder.jp/contests/abc157/tasks/abc157_a"
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    if(n%2)
+        cout<<n/2+1;
+    else
+        cout<<n/2;
+
+        
+    return 0;
+}
