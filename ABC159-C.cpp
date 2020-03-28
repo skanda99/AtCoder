2f705302df3a02a357d29ba72c2839bbb2cd0dc9
skanda99 <skandaupadhyaya1999@gmail.com>
2020-03-28 12:26:18 +0530

    Maximum Volume

---
+++ ABC159-C.cpp
@@ -0,0 +1,17 @@
+
+// problem: "https://atcoder.jp/contests/abc159/tasks/abc159_c"
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    double l;
+    cin>>l;
+
+    cout<<fixed;
+    cout<<setprecision(12)<<pow(l/3.0,3);
+
+    return 0;
+}
