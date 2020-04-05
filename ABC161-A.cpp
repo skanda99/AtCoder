ae3988d519c1c150c5a0f84c3a62e302fae2c191
skanda99 <skandaupadhyaya1999@gmail.com>
2020-04-05 12:27:29 +0530

    ABC Swap

---
+++ ABC161-A.cpp
@@ -0,0 +1,15 @@
+
+// problem: "https://atcoder.jp/contests/abc161/tasks/abc161_a"
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int x,y,z;
+    cin>>x>>y>>z;
+    cout<<z<<' '<<x<<' '<<y;
+
+    return 0;
+}
